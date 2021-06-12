951260249ef89b7f53b45f0797aa3811493b8798
Roberto Zegers <rzegers@uc.cl>
2021-06-12 16:37:45 +0200

    Feat: enable creation of new venues, inserts form data as a new Venue record in the db

--- app.py
+++ app.py
@@ -169,14 +169,28 @@ def create_venue_form():
 
 @app.route('/venues/create', methods=['POST'])
 def create_venue_submission():
-  # TODO: insert form data as a new Venue record in the db, instead
-  # TODO: modify data to be the data object returned from db insertion
-
-  # on successful db insert, flash success
-  flash('Venue ' + request.form['name'] + ' was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Venue ' + data.name + ' could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+  form = VenueForm(request.form)
+  error = False
+  try:
+    venue = Venue(name = form.name.data,
+                  city = form.city.data,
+                  state = form.state.data,
+                  address = form.address.data,
+                  phone = form.phone.data,
+                  genres = form.genres.data,
+                  image_link = form.image_link.data,
+                  seeking_talent = form.seeking_talent.data,
+                  seeking_description = form.seeking_description.data)
+    db.session.add(venue)
+    db.session.commit()
+    flash('Venue ' + request.form['name'] + ' was successfully listed!')
+  except:
+      error = True
+      db.session.rollback()
+      flash('An error occured. Venue ' + request.form['name'] + ' could not be listed!')
+  finally:
+      db.session.close()
+  
   return render_template('pages/home.html')
 
 @app.route('/venues/<venue_id>', methods=['DELETE'])
