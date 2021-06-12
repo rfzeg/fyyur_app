cfe7f0281319b4ec62f927fc8118a90af3440162
Roberto Zegers <rzegers@uc.cl>
2021-06-12 23:13:34 +0200

    Feat: enable creation of new shows, inserts form data as a new Show record in the db

--- app.py
+++ app.py
@@ -346,14 +346,21 @@ def create_shows():
 
 @app.route('/shows/create', methods=['POST'])
 def create_show_submission():
-  # called to create new shows in the db, upon submitting new show listing form
-  # TODO: insert form data as a new Show record in the db, instead
-
-  # on successful db insert, flash success
-  flash('Show was successfully listed!')
-  # TODO: on unsuccessful db insert, flash an error instead.
-  # e.g., flash('An error occurred. Show could not be listed.')
-  # see: http://flask.pocoo.org/docs/1.0/patterns/flashing/
+  form = ShowForm(request.form)
+  error = False
+  try:
+    show = Show(start_time = form.start_time.data,
+                parent_artist_id = form.artist_id.data,
+                parent_venue_id = form.venue_id.data)
+    db.session.add(show)
+    db.session.commit()
+    flash('Show was successfully listed!')
+  except:
+      error = True
+      db.session.rollback()
+      flash('An error occured. Show could not be listed!')
+  finally:
+      db.session.close()
   return render_template('pages/home.html')
 
 @app.errorhandler(404)
